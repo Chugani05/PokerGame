05511197e583d3849cdbf8880b9cfa3d14bea9ae
Chugani05 <chugani205@gmail.com>
2024-05-15 00:08:56 +0100

    .

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
-class Game:
-    def __init__():
+from __future__ import annotations
 
-    
-    def get_winner(
-    players: list[Player],
-    common_cards: list[Card],
-    private_cards: list[list[Card]],
-) -> tuple[Player | None, Hand]:
+import roles
+
+
+class Game:
+
+    def __init__(self, players, common_cards, private_cards):
+        self.players = players
+        self.common_cards = common_cards
+        self.private_cards = private_cards
+
+    def get_winner( players: list[Player], common_cards: list[Card], private_cards: list[list[Card]]) -> tuple[Player | None, Hand]:
+        ...
