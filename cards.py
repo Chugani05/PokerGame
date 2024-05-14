cb05b559e8899d4af9e11b6068970dfa1b9f7343
nicholelouis <nicholelouiss@gmail.com>
2024-05-14 14:46:00 +0100

    avance proyecto

--- cards.py
+++ cards.py
@@ -1,22 +1,63 @@
+from __future__ import annotations
+import random
+import helpers
+
 class Card:
-    def __init__():
+    values = {'J' : 11, 'Q' : 12, 'K': 13, 'A' : 14}
+    def __init__(self, card):
+        self.suit = card[-1]
+        self.pinta = card[0:-1]
+        self.value = self.values[self.pinta] if self.pinta in self.values else int(self.pinta)
 
-
+    def __gt__(self, other: Card) -> bool:
+        return self.value > other.value
+    
+    def __eq__(self, other: Card) -> bool:
+        return self.value == other.value
+    
+    def __str__(self):
+        return f'{self.pinta + self.suit}'
+    
+    def __repr__(self) -> str:
+        return f'{self.pinta + self.suit}'
+    
 class Deck:
-    def __init__():
+    suits = ['♠', '♣', '◆', '❤']
+    letters = ['J', 'Q', 'K', 'A']
+    def __init__(self):
+        self.deck = []
+        self.generate()
+        self.shuffle_deck()
 
+    def generate(self):
+        for suit in self.suits:
+            for num in range(2, 11):
+                self.deck.append(Card(str(num) + suit))
+            for n in range(4):
+                self.deck.append(Card(self.letters[n] + suit))
+    
+    def shuffle_deck (self):
+        random.shuffle(self.deck)
 
 class Hand:
-    def __init__():
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR_OF_A_KIND = 8
+    STRAIGHT_FLUSH = 9
 
-    def __contains__():
+    def __init__(self, cards):
+        self.cards = cards
+        self.moves()
 
-    HIGH_CARD = 0
-    ONE_PAIR = 1
-    TWO_PAIR = 2
-    THREE_OF_A_KIND = 3
-    STRAIGHT = 4
-    FLUSH = 5
-    FULL_HOUSE = 6
-    FOUR_OF_A_KIND = 7
-    STRAIGHT_FLUSH = 8
+    def moves(self):
+        return list(helpers.combinations(self.cards, n=5))
+    
+    def find_move(self):
+        ... 
+        
+    
--- game.py
+++ game.py
@@ -1,9 +1,12 @@
-class Game:
-    def __init__():
+from __future__ import annotations
+import roles
 
-    
-    def get_winner(
-    players: list[Player],
-    common_cards: list[Card],
-    private_cards: list[list[Card]],
-) -> tuple[Player | None, Hand]:
+class Game:
+
+    def __init__(self, players, common_cards, private_cards):
+        self.players = players
+        self.common_cards = common_cards
+        self.private_cards = private_cards
+
+    def get_winner( players: list[Player], common_cards: list[Card], private_cards: list[list[Card]]) -> tuple[Player | None, Hand]:
+        ...
