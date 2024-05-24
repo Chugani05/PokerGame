05cceaa25dca46c3dfd96764d3a5d61393607991
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-05-25 00:44:57 +0100

    Update roles_prueba.py

--- roles_prueba.py
+++ roles_prueba.py
@@ -6,10 +6,9 @@ class Dealer:
         self.player1 = player1
         self.player2 = player2
     
-    def reveal_cards(self) -> List[str]:
+    def reveal_community_cards(self) -> List[str]:
         community_cards = [self.deck.pop(0) for _ in range(5)]
-        self.player1.set_common_cards(community_cards)
-        self.player2.set_common_cards(community_cards)
+        Player.receive_common_cards(community_cards)
         return community_cards
 
     def deal_private_cards(self):
@@ -26,16 +25,18 @@ class Dealer:
             return (self.player2.name, hand2)
 
 class Player:
+    common_cards = []
+
     def __init__(self, name: str):
         self.name = name
-        self.private_cards: List[str] = []
-        self.common_cards: List[str] = []
+        self.private_cards = []
 
     def receive_private_cards(self, cards: List[str]):
         self.private_cards = cards
 
-    def receive_community_cards(self, cards: List[str]):
-        self.common_cards = cards
+    @classmethod
+    def receive_community_cards(cls, cards: List[str]):
+        cls.common_cards = cards
 
     def get_best_hand(self) -> List[str]:
         return sorted(self.private_cards + self.common_cards, reverse=True)[:5]
