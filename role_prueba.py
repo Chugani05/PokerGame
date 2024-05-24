ee5a6d6fa75be40b1b409eef9336a6c85aa449d5
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-05-24 23:35:51 +0100

    Create role_prueba.py

---
+++ role_prueba.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+
+class Dealer:
+    def __init__(self, deck: List[str], player1: Player, player2: Player):
+        self.deck = deck
+        self.player1 = player1
+        self.player2 = player2
+    
+    def reveal_cards(self) -> List[str]:
+        community_cards = [self.deck.pop(0) for _ in range(5)]
+        self.player1.set_common_cards(community_cards)
+        self.player2.set_common_cards(community_cards)
+        return community_cards
+
+    def deal_private_cards(self):
+        self.player1.set_private_cards([self.deck.pop(0) for _ in range(2)])
+        self.player2.set_private_cards([self.deck.pop(0) for _ in range(2)])
+
+    def best_hands(self) -> Tuple[str, List[str]]:
+        hand1 = self.player1.best_hand()
+        hand2 = self.player2.best_hand()
+        
+        if hand1 > hand2:
+            return (self.player1.name, hand1)
+        else:
+            return (self.player2.name, hand2)
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.private_cards: List[str] = []
+        self.common_cards: List[str] = []
+
+    def set_private_cards(self, cards: List[str]):
+        self.private_cards = cards
+
+    def set_common_cards(self, cards: List[str]):
+        self.common_cards = cards
+
+    def get_hand(self) -> List[str]:
+        return self.private_cards + self.common_cards
+
+    def best_hand(self) -> List[str]:
+        return sorted(self.get_hand(), reverse=True)
